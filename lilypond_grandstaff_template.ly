5028e3fd306cbf48d5819f12be1cf41260fdb85a
Michael Sarfati <msarfati@protonmail.com>
2020-01-19 01:41:03 -0500

    grand staff generation for lilypond

---
+++ lilypond_grandstaff_template.ly
@@ -0,0 +1,12 @@
+\version "2.18.2"
+\header {
+  title = "Random Drills"
+  composer = "Michael Sarfati"
+}
+
+\absolute {
+	<<
+		\new Staff { \clef "treble" %REPLACE_TREBLE1%  }
+		\new Staff { \clef "bass" %REPLACE_BASS1% }
+	>>
+}
---
+++ sheet_music_gen.py
@@ -0,0 +1,87 @@
+#!/usr/bin/env python3
+import itertools
+import random
+import sys
+import time
+
+notes = [
+    'c',
+    'd',
+    'e',
+    'f',
+    'g',
+    'a',
+    'b',
+]
+
+accidentals = [
+    '',
+    '',
+    '',
+    '',
+    '',
+    'is',  # sharp
+    'es',  # flat
+]
+
+time_value = [
+    # '1',
+    # '2',
+    # '3',
+    '4',
+]
+
+treble_octaves = [
+    "'",
+    "''",
+    # "'''",
+]
+
+bass_octaves = [
+    ",",
+    "",
+]
+
+def gen_bar(clef='treble'):
+    return " ".join([make_note(clef) for i in range(4)]) + " | "
+
+
+def make_note(clef='treble'):
+    note = random.choice(notes)
+    accidental = random.choice(accidentals)
+    note_len = random.choice(time_value)
+
+    if clef == 'bass':
+        octave = random.choice(bass_octaves)
+    else:
+        octave = random.choice(treble_octaves)
+
+    # print(octave)
+    # print(type(octave))
+    return note + accidental + octave + note_len
+
+
+def gen_staff(clef='treble', bars=40):
+    return "".join([gen_bar(clef) for i in range(bars + 1)])
+
+
+def main():
+
+    template_path = './lilypond_grandstaff_template.ly'
+    template = ''
+
+    with open(template_path, 'r+') as f_template:
+        template = f_template.read()
+        f_template.close()
+
+    template = template.replace("%REPLACE_TREBLE1%", gen_staff('treble'))
+    template = template.replace("%REPLACE_BASS1%", gen_staff('bass'))
+
+    ly_out_path = str(time.time()).replace('.', '_') + ".ly"
+    with open(ly_out_path, "w+") as ly_out:
+        ly_out.write(template)
+        ly_out.close()
+
+
+if __name__ == "__main__":
+    main()
